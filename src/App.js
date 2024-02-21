24635c021dc310909ed87aaa3f906e4017c52a71
saigenix <sainathmahindrakar@gmail.com>
2024-02-22 01:19:51 +0530

    update2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/App.css";
 import React from "react";
 import { useRef } from "react";
-
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Link } from "react-scroll";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -9,11 +9,22 @@ import Project from "./pages/Project";
 import Skill from "./pages/Skill";
 import Contact from "./pages/Contact";
 import Blank from "./pages/Blank";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 function App() {
     return (
-        <div className="App">
-            <Home />
+        <div id="smooth-wrapper">
+            <div id="smooth-content">
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Home />} />
+                        <Route path="About" element={<About />} />
+                    </Route>
+                </Routes>
+            </div>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,93 @@ import React from "react";
 import Contact from "../images/Contact.png";
 import "../styles/Home.css";
 import cursor from "../images/cursor.png";
+import l1 from "../images/l1.png";
+import snow from "../images/snow.png";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import Blank from "./Blank";
+
 const Home = () => {
+    const snowRef = useRef();
+    const container = useRef();
+
+    useGSAP(
+        () => {
+            // gsap code here...
+            const t1 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".title",
+                    scrub: 1,
+                    pin: container.current,
+                    markers: true,
+                    start: "top-=500 top",
+                    end: "+=100%",
+                },
+            });
+            gsap.from(".scroll", {
+                scrollTrigger: {
+                    trigger: ".scroll",
+                    markers: true,
+                    scrub: true,
+                    start: "top-=300 top",
+                    end: "+=30px",
+                },
+                opacity: 1,
+                duration: 0.1,
+            });
+            t1.from(".title", {
+                y: "400px",
+                opacity: 0,
+                // rotate: "50deg",
+                // scale:50,
+
+                duration: 5,
+                ease: "power1.out",
+                stagger: 0.3,
+            }); // <-- automatically reverted
+
+            t1.fromTo(
+                ".snow",
+                {
+                    y: "-100%",
+                    x: "-100px",
+                },
+                {
+                    y: "0px",
+                    x: "0px",
+                    duration: 5,
+                }
+            );
+        },
+        { scope: container }
+    );
+
     return (
-        <div className="app h-[100%] w-[100%] flex justify-center flex-col">
-            <div className="flex flex-col place-self-center justify-center content-center animate-bounce duration-1000">
-                <span className=" text-xs pb-2">Scroll To See...</span>
-                <img src={cursor} alt="img" className="h-14 w-14 ml-10" />
+        <>
+            <div className="App" ref={container}>
+                <div className="scroll  opacity-0 absolute flex flex-col place-self-center justify-center content-center animate-bounce duration-1000 z-10">
+                    <span className=" text-xs pb-2">Scroll To See...</span>
+                    <img src={cursor} alt="img" className="h-14 w-14 ml-10" />
+                </div>
+                <img
+                    src={snow}
+                    alt="img"
+                    className="snow absolute z-[1] object-contain h-[100%] w-[100vw]"
+                />
+                <div className="paper z-[0] pwhite title">
+                    <span className=" absolute translate-x-[-20vw] translate-y-[15vh] text-[25px] text-[#003858]">
+                        Hey, I am
+                    </span>
+                    <h1 className="text-[20vw] text-[#FD3044] p-0 m-0 relief">
+                        Saigenix
+                    </h1>
+                </div>
             </div>
-            <div className="paper">
-                <span className=" absolute translate-x-[-20vw] translate-y-[15vh] text-[25px] text-[#003858]">
-                    Hey, I am
-                </span>
-                <h1 className="text-[20vw] text-[#FD3044] p-0 m-0 relief">
-                    Saigenix
-                </h1>
-                
-            </div>
-        </div>
+            {/* <div className="">
+                <Blank />
+            </div> */}
+        </>
     );
 };
 
